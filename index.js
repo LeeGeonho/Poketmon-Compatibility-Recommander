1ed8672e5097493083b96cb2d79d9e1216326df1
GeonhoLee <leesb86@prunebomb.com>
2023-05-19 15:10:19 +0900

    findByTeraType

--- index.js
+++ index.js
@@ -1,61 +1,104 @@
-const { COMPATIBILITY, RAID_MONSTERS, USER_MONSTERS } = require("./type");
+const { COMPATIBILITY, ATTR } = require("./types");
+const { RAID_MONSTERS, USER_MONSTERS } = require("./monsters");
 
-const find = (name) => {
-  if (!RAID_MONSTERS[name]) return;
+// 테라스탈 타입기준 상성이 되는 유리한 사용자 포켓몬을 찾는다.
+const findUserMonster = (teraType) => {
+  const compatibility = COMPATIBILITY[teraType];
+  const entryMonsters = { 0: [], 1: [] };
 
-  const { tera, skillType } = RAID_MONSTERS[name];
-
-  // 활성화된 테라스탈에 상성이 되는 속성을 찾는다.
-  const compatibility = COMPATIBILITY[tera];
-  // console.log("효과가 굉장한 속성:", compatibility[0]);
-
-  let entryMonsters = [];
   Object.entries(USER_MONSTERS).map(async (monster) => {
     const name = monster[0];
     const attr = monster[1];
     // console.log(name, attr);
 
-    // 효과가 굉장한 사용자 몬스터가 있는지
-    const recommandtype = compatibility[0].filter((item) =>
-      attr.type.includes(item)
-    );
-    if (recommandtype.length > 0) {
-      entryMonsters.push(name);
-    }
+    // 0: 효과가 굉장함
+    // 1: 효과가 있음
+    [0, 1].map((stage) => {
+      const recommand = compatibility[stage].filter((item) =>
+        attr.type.includes(item)
+      );
+
+      if (recommand.length > 0) {
+        entryMonsters[stage].push(name);
+      }
+    });
   });
 
-  // 방어 상성이 있는지
-  if (entryMonsters.length > 0) {
-    let dangerCount = 0;
-
-    entryMonsters = entryMonsters.map((name) => {
-      const { dangerType } = USER_MONSTERS[name];
-
-      const found = skillType.filter((item) => dangerType.includes(item));
-
-      if (found.length > 0) {
-        dangerCount++;
-        if (dangerCount >= 2) {
-          return null;
-        }
-      }
-
-      return name;
-    });
+  // console.log("entryMonsters", entryMonsters);
+  if (entryMonsters[0].length === 0 && entryMonsters[1].length === 0) {
+    return [false];
   }
+
+  return [true, entryMonsters];
+};
+
+// 레이드 몬스터의 기술 속성이 사용자 몬스터와 상성인지 체크
+const findByDefenceType = (skillType, monsters, stage) => {
+  let dangerCount = 0;
+
+  const entryMonsters = monsters[stage].map((name) => {
+    const { dangerType } = USER_MONSTERS[name];
+
+    const found = skillType.filter((item) => dangerType.includes(item));
+
+    if (found.length > 0) {
+      // 상성인 기술이 2개 이상이면 entry에서 제외
+      if (++dangerCount >= 2) {
+        return null;
+      }
+    }
+
+    return name;
+  });
 
   return entryMonsters.filter((item) => item !== null);
 };
 
-(() => {
-  const recommandMonster = find("브리가론");
+const findByMonster = (name) => {
+  if (!RAID_MONSTERS[name]) return;
 
-  if (recommandMonster.length === 0) {
+  const { teraType, skillType } = RAID_MONSTERS[name];
+
+  const [found, monsters] = findUserMonster(teraType);
+
+  if (!found) {
+    return [null];
+  }
+
+  let stage = 0;
+  let finalEntry = findByDefenceType(skillType, monsters, stage);
+  if (finalEntry.length === 0) {
+    // 효과가 굉장한 애들 모두 방어 상성을 갖고있는 경우 효과가 있는 애들로 다시 검색
+    stage = 1;
+    finalEntry = findByDefenceType(skillType, monsters, stage);
+  }
+
+  return [stage, finalEntry];
+};
+
+const findByTeraType = (teraType) => {
+  const [found, monsters] = findUserMonster(teraType);
+
+  if (!found) {
+    return [null];
+  }
+
+  return [0, monsters[0]];
+};
+
+(() => {
+  const [stage, entry] = findByMonster("브리가론");
+  // const [stage, entry] = findByTeraType(ATTR.FLY);
+
+  if (!!stage || entry.length === 0) {
     console.log("not entries...");
     return;
   }
-  console.log("Recommand monsters are...");
-  for (const monster of recommandMonster) {
+
+  console.log(
+    `--------- ${stage == 0 ? `효과가 굉장한` : `효과가 있는`} 몬스터 목록`
+  );
+  for (const monster of entry) {
     console.log(monster);
   }
 })();
